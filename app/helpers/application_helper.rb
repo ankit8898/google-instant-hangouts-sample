9d01cfee1e2580768e0812146e00bd8dd51cad31
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-01-27 17:09:50 -0500

    Sample App

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,24 @@
+module ApplicationHelper
+
+	# def instant_hangout(opts={})
+	# 	defaults = {render: 'createhangout',lang: 'en',room_id: 'my_room',topic: 'my topic',width: 200}
+	# 	defaults.merge!(opts)
+	# 	content_tag(:div,
+	# 	    nil,
+	# 	    class: "instanthangouts",
+	# 		render: defaults[:render],
+	# 		lang: defaults[:lang],
+	# 		room_id: defaults[:room_id],
+	# 		topic: defaults[:topic],
+	# 		width: defaults[:width])
+	# end
+end
+
+
+ # lang='fr'
+ #    parsetags='onload'
+ #    publisher_id='112744459749475398119'
+ #    render='hangout'
+ #    room_id='my_room'
+ #    topic='My Topic'
+ #    width='200
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleHangout::Application.config.secret_key_base = 'f12afe8be99468cf0100f2085e0fba8ad0b645715e59577eb4b36f42bb7a914f44b0239ab276a48f7a8c771f8ee54e5d0ea5b169948205b5f165c43e253eee6f'
